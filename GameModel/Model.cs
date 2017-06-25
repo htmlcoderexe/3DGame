e6ffc9fb81dc7a4f803f22ac4bbfbe039ae45cff
admin <admin@MSI>
2017-06-25 20:57:41 +0200

    Revert "Entities adjust to terrain correctly. X/Z swap against XNA, for rendering roll and pitch swapped"
    This reverts commit 73db12f083569530849ce5fc539b669424fd5743.

--- GameModel/Model.cs
+++ GameModel/Model.cs
@@ -8,50 +8,97 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameModel
 {
-    public class Model
+    public class Model : ModelPart
     {
-        private VertexBuffer _vertices;
-        private IndexBuffer _indices;
+        private VertexBuffer _VB;
+        private IndexBuffer _IB;
         private void Prepare(GraphicsDevice device)
         {
-            
-            _indices = new IndexBuffer(device, IndexElementSize.ThirtyTwoBits, 36, BufferUsage.WriteOnly);
-            _indices.SetData<int>(new int[] { //--,++,-+/--,+-,++
-                 0, 1, 2, 0, 3, 1 
+            Vector2 TotalSizes = this.Children[0].GetCounts();
+
+
+            _IB = new IndexBuffer(device, IndexElementSize.ThirtyTwoBits, (int)TotalSizes.Y, BufferUsage.WriteOnly);
+            _VB = new VertexBuffer(device, VertexPositionColor.VertexDeclaration, (int)TotalSizes.X, BufferUsage.WriteOnly);
+            int[] indices = new int[(int)TotalSizes.Y];
+            VertexPositionColor[] vertices = new VertexPositionColor[(int)TotalSizes.X];
+            //current vertex/index offset
+            int cIo = 0;
+            int cVo = 0;
+            List<ModelPart> Parts = this.Children[0].GetFlatList();
+            foreach(ModelPart p in Parts)
+            {
+                int Icount = p.IndexLength;
+                int Vcount = p.VertexLength;
+                p.IndexOffset = cIo;
+                p.VertexOffset = cVo;
+                p.GetIndices().CopyTo(indices, cIo);
+                p.GetVertices().CopyTo(vertices, cVo);
+                cIo += Icount;
+                cVo += Vcount;
+            }
+
+
+            _IB.SetData<int>(indices);
+            _VB.SetData<VertexPositionColor>(vertices);
+
+        }
+        public Model()
+        {
+            this.Children = new List<ModelPart>();
+            ModelPart Root = new ModelPart();
+
+            VertexPositionColor[] vertices = new VertexPositionColor[8];
+            vertices[0] = new VertexPositionColor(new Vector3(-1.5f,  0.5f, -0.5f), Color.DarkGray );
+            vertices[1] = new VertexPositionColor(new Vector3( 0.5f,  0.2f,  0.2f), Color.DarkGray );
+            vertices[2] = new VertexPositionColor(new Vector3(-1.5f,  0.5f,  0.5f), Color.DarkGray );
+            vertices[3] = new VertexPositionColor(new Vector3( 0.5f,  0.2f, -0.2f), Color.DarkGray );
+                                                                                         
+            vertices[4] = new VertexPositionColor(new Vector3(-1.5f, -0.5f, -0.5f), Color.DarkGray );
+            vertices[5] = new VertexPositionColor(new Vector3( 0.5f, -0.2f,  0.2f), Color.DarkGray );
+            vertices[6] = new VertexPositionColor(new Vector3(-1.5f, -0.5f,  0.5f), Color.DarkGray );
+            vertices[7] = new VertexPositionColor(new Vector3( 0.5f, -0.2f, -0.2f), Color.DarkGray );
+            Root.SetVertices(vertices);
+            Root.SetIndices(new int[] { //--,++,-+/--,+-,++
+                 0, 1, 2, 0, 3, 1
                 ,4, 6, 5, 4, 5, 7
                 ,4, 2, 6, 4, 0, 2
                 ,7, 5, 1, 7, 1, 3
                 ,6, 2, 1, 6, 1, 5
                 ,4, 3, 0, 4, 7, 3
             });
-            VertexPositionColor[] vertices = new VertexPositionColor[8];
-            vertices[0] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, -0.5f), Color.Blue);
-            vertices[1] = new VertexPositionColor(new Vector3(0.5f, 0.5f, 0.5f), Color.Blue);
-            vertices[2] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, 0.5f), Color.Blue);
-            vertices[3] = new VertexPositionColor(new Vector3(0.5f, 0.5f, -0.5f), Color.Blue);
 
-            vertices[4] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, -0.5f), Color.Green);
-            vertices[5] = new VertexPositionColor(new Vector3(0.5f, -0.5f, 0.5f), Color.Yellow);
-            vertices[6] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, 0.5f), Color.Red);
-            vertices[7] = new VertexPositionColor(new Vector3(0.5f, -0.5f, -0.5f), Color.Green);
-            VertexDeclaration dd = VertexPositionColor.VertexDeclaration;
-            _vertices = new VertexBuffer(device, dd, 8, BufferUsage.WriteOnly);
-            _vertices.SetData<VertexPositionColor>(vertices);
+            ModelPart leg;
+            leg = new TestParts.PartBugLeg();
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 0.3f) * Matrix.CreateTranslation(new Vector3(0.4f, 0, -0.5f)));
+            leg = new TestParts.PartBugLeg();
+            leg.Animation.SetPhase(0.5f);
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 0.5f) * Matrix.CreateTranslation(new Vector3(0, 0, -0.5f)));
+            leg = new TestParts.PartBugLeg();
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 0.8f) * Matrix.CreateTranslation(new Vector3(-0.4f, 0, -0.5f)));
+
+            leg = new TestParts.PartBugLeg(true);
+            leg.Animation.SetPhase(0.5f);
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 1.8f) * Matrix.CreateTranslation(new Vector3(0.4f, 0, 0.5f)));
+            leg = new TestParts.PartBugLeg(true);
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 1.5f) * Matrix.CreateTranslation(new Vector3(0, 0, 0.5f)));
+            leg = new TestParts.PartBugLeg(true);
+            leg.Animation.SetPhase(0.5f);
+            Root.Append(leg, Matrix.CreateRotationY((float)Math.PI * 1.3f) * Matrix.CreateTranslation(new Vector3(-0.4f, 0, 0.5f)));
+
+
+            this.Children.Add(Root);
 
         }
-        public Model()
+        public override void Render(GraphicsDevice device, float dT, Matrix World, BasicEffect fx)
         {
-           
-        }
-        public void Render(GraphicsDevice device, float dT, Matrix World, BasicEffect fx)
-        {
-            if (this._indices == null)
+            if (this._IB == null)
                 Prepare(device);
-            fx.World = World;
-            fx.CurrentTechnique.Passes[0].Apply();
-            device.SetVertexBuffer(_vertices);
-            device.Indices = _indices;
-            device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 12);
+            device.SetVertexBuffer(_VB);
+            device.Indices = _IB;
+            foreach(ModelPart c in this.Children)
+            {
+                c.Render(device, dT, World, fx);
+            }
 
         }
 
