378374ed314513e1dd9ef184009319bc33e5fa79
htmlcoderexe <unknown@nona.me>
2020-03-01 00:38:27 +0100

    fuck it,, some kind of a class is a beginning

---
+++ GameModel/Choreo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    public class Choreo
+    {
+        Dictionary<string, Dictionary<string, PartAnimation>> AnimationData;
+
+        public Choreo(Dictionary<string, Dictionary<string, PartAnimation>> Data)
+        {
+            this.AnimationData = Data;
+        }
+
+        public Dictionary<string, PartAnimation> GetMove(string Name)
+        {
+            if (!this.AnimationData.ContainsKey(Name))
+                return null;
+            return this.AnimationData[Name];
+        }
+
+        public float GetAnimationLength(string Name)
+        {
+            float max = 0;
+
+            Dictionary<string, PartAnimation> Move = GetMove(Name);
+            if (Move != null)
+                foreach (PartAnimation p in Move.Values.ToList())
+                    if (p.Length > max)
+                        max = p.Length;
+
+            return max;
+        }
+
+
+    }
+}
